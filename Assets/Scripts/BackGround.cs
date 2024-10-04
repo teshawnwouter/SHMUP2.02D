6c944873f653d30e5b55e3ccd5cae930eebee627
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2024-10-04 19:08:22 +0200

    moving background perfected

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -1,30 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;  
+using UnityEngine;
+using UnityEngine.UI;
 
 public class BackGround : MonoBehaviour
 {
-    [SerializeField] GameObject[] bg;
-    float speed = 2f;
+    
 
-    [SerializeField]float offSet = 15f;
-    void Start()
-    {
+    [SerializeField]private RawImage backgroundIMG;
+    private float ySpeed = 1f;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(Screen.height);
-        for (int i = 0; i < bg.Length; i++) 
-        { 
-            bg[i].transform.position += new Vector3(0, 1 * speed * Time.deltaTime, 0);
-            if (bg[i].transform.position.y > offSet)
-            {
-                Debug.Log("hallo");
-                bg[i].transform.position = new Vector3(0,-offSet, 0);
-            }
-        }
+        backgroundIMG.uvRect = new  Rect(backgroundIMG.uvRect.position + new Vector2(0,ySpeed) * Time.deltaTime, backgroundIMG.uvRect.size); 
     }
 }
